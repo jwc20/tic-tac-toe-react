e27e59b6d671146b20361ebe29709ff04dff1fc0
jwc20 <choij2015@gmail.com>
2022-08-03 16:11:17 -0700

    update the Board component to have it receive squares and onClick props from the Game components (will produce errors)

--- src/index.js
+++ src/index.js
@@ -11,13 +11,15 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     squares: Array(9).fill(null),
+  //     xIsNext: true,
+  //   };
+  // }
+
+
 
   handleClick(i) {
     const squares = this.state.squares.slice(); // creating a copy of the squares for immutable data
@@ -31,8 +33,8 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
