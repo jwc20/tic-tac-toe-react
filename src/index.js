df736aa5a7de9c075a063e7518f17fd39cdee2e0
jwc20 <choij2015@gmail.com>
2022-08-03 13:28:54 -0700

    each Square will now reeive value prop that will either be "X" or "O" or null for empty squares

--- src/index.js
+++ src/index.js
@@ -28,7 +28,8 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={i} />;
+    // return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
