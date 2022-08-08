ecb2306b691d1644ae2f64e0beb75b6eaa2374e3
jwc20 <choij2015@gmail.com>
2022-08-07 17:15:16 -0700

    refactor, separate files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Game from "./Game";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
